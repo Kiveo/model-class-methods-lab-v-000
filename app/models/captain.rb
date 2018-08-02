194f8c1fb16ad6edf0a5f0d59b941d4161251f52
Maverick Tesini <mavericktesini@gmail.com>
2018-08-02 06:02:32 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,6 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    #EX: User.includes(:posts).where('posts.name = ?', 'example').references(:posts)
     includes(boats: :classifications).where('classifications.name = ?', 'Catamaran').references(:classifications)
   end
 
