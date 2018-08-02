9fdc1093c29808ab9a938914b9655efe2500e22d
Maverick Tesini <mavericktesini@gmail.com>
2018-08-02 05:08:06 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,8 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators #where captain > boats > boat_classifications = catamaran
-
+    captain_boats = boats.collect {|boat| boat.captain_id == id}
+    # captain_boats compare to boat_classifications
   end
 
 end
