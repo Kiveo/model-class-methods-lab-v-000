2a858d8d7c1e29ba3ef162081578a64817e6f61e
Maverick Tesini <mavericktesini@gmail.com>
2018-08-02 05:26:20 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -5,8 +5,11 @@ class Captain < ActiveRecord::Base
     capt_boats = boats.collect {|boat| boat.captain_id == id}
   end
 
-  def self.catamaran_operators #where captain > boats > boat_classifications = catamaran
-    # captain_boats compare to boat_classifications that contain the classification_id == catamaran
+  def self.catamaran_operators 
+    #captain -has_many- boats -has_many- boat_classifications -belongs_to- classification where name == catamaran
+    # captain_boats compare to boat_classifications 
+    # that contain the classification.catamaran_ids
+    #where b_c has catamaran_ids , see if any == captain_boats, return boats that do
   end
 
 end
