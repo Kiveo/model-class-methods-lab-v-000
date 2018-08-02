8810e32c1261e06c485403a1de1575c2284a13e7
Maverick Tesini <mavericktesini@gmail.com>
2018-08-02 06:04:28 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,7 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    includes(boats: :classifications).where('classifications.name = ?', 'Catamaran').references(:classifications)
+    includes(boats: :classifications).where('classifications.name = ?', 'Catamaran')
   end
 
   def self.sailors #returns captains with sailboats
