0c8bde875de9ca51fb0bbdfe125a5c1b8b407f8a
Maverick Tesini <mavericktesini@gmail.com>
2018-08-02 06:06:30 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,4 +9,8 @@ class Captain < ActiveRecord::Base
     includes(boats: :classifications).where('classifications.name = ?', 'Sailboat').uniq
   end
 
+  def self.talented_seafarers #returns captains of motorboats and sailboats('sailors')
+    includes(boats: :classifications).where('classifications.name = ?' 'Sailboat, Motorboat')       
+  end
+  
 end
