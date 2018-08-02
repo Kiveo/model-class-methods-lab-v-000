06e4032d317110c853c293c69a347c3f99b93029
Maverick Tesini <mavericktesini@gmail.com>
2018-08-02 04:52:32 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -19,7 +19,7 @@ class Boat < ActiveRecord::Base
   end
   def self.last_three_alphabetically
     a = order(:name)
-    a.limit(3).order('name desc')
+    a.limit(3).order('name asc')
   end
 
 end
