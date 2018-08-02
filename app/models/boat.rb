7c544a5166fc6280aee32c7fc63b649cffdc86cc
Maverick Tesini <mavericktesini@gmail.com>
2018-08-02 04:36:12 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,9 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    limit(5).order('id desc')
+  end
+  
 end
